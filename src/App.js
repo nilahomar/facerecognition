d4cd906dd6fdf97c3d9ee25d51bdc22c110cd49f
Nila Homar <nilahomar@gmail.com>
2022-04-26 22:28:58 +0200

    all components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import 'tachyons';
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import './App.css';
+import particlesOptions from "./particles.json";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/* <ImageLinkForm />
-      <faceRecognition /> */}
-    </div>
-  );
+    const particlesInit = useCallback(main => {
+        loadFull(main);
+    }, [])
+
+    return (
+        <div className="App">
+            <Particles options={particlesOptions} init={particlesInit} />
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForm />
+            {/* <faceRecognition /> */}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
 import './Logo.css'
 
 function Logo() {
   return (
     <div className='ma4 mt0'>
-      <Tilt className='Tilt' style={{ height: '150px', width: '150px'}}>
-        <div >
-          <h1> 👀</h1>
+      <Tilt className='Tilt' style={{ height: '150px', width: '150px' }}>
+        <div className='pa3'>
+          <img style={{ paddingTop: '8px' }} src={brain} alt='logo' />
         </div>
       </Tilt>
     </div>
